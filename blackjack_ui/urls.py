5c8042b8047c3d17278f1aa4db5f5e271e43cd64
Frank Wiles <frank@revsys.com>
2019-10-23 19:14:29 -0500

    Update url structure a touch

--- blackjack_ui/urls.py
+++ blackjack_ui/urls.py
@@ -5,12 +5,8 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views.static import serve
 
-from users import views as users_views
-from users import urls as users_urls
-
 urlpatterns = [
-    url(r"^api/callback", users_views.CallbackAPIView.as_view()),
-    url(r"^api/", include((users_urls, "users_api"))),
+    url(r"^api/", include(("users.urls", "users_api"))),
     url(r"^admin/", admin.site.urls),
     re_path(
         r"^static/(?P<path>.*)$",
--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 
 from rest_auth.views import LogoutView
 
-from .views import LoginView, UserDetailView, RegisterView
+from .views import LoginView, UserDetailView, RegisterView, CallbackAPIView
 
 
 urlpatterns = [
+    url(r"^callback", CallbackAPIView.as_view(), name="callback"),
     url(r"^login/$", LoginView.as_view(), name="login"),
     url(r"^logout/$", LogoutView.as_view(), name="logout"),
     url(r"^register/$", RegisterView.as_view(), name="register"),
